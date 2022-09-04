12d2025855d22f95d85b65d4f60fb2198eaa2b5c
saif0726 <112571017+saif0726@users.noreply.github.com>
2022-09-04 20:36:58 +0530

    Add files via upload

---
+++ Day_02.py
@@ -0,0 +1,20 @@
+# print('print(hello python)')
+# print("print(hello python)")
+# print('''print(hello python)''')
+# print("""print(hello python)""")
+
+# print('hello world')
+
+# print('''Hello 
+#         world''')
+
+# print('''Hello
+
+#          world''')
+
+# print('''hello python''')
+# print("""hello python""")
+
+# print('hello pythhon')
+# print('''hello python''')
+# print("""hello python""") 
